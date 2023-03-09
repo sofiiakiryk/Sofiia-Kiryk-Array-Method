b9fbd7ae422e097f186cec0021326d2af9b5a76d
sofiiakiryk <107884647+sofiiakiryk@users.noreply.github.com>
2023-03-09 16:30:48 +0300

    Add files via upload

---
+++ array.js
@@ -0,0 +1,9 @@
+let num = [];
+let numbers;
+for (let i = 0; i < 7; i++) {
+  number = Number(prompt(`Enter number`));
+  num.push(number);
+}
+console.log(num[0]);
+console.log(num[6]);
+console.log(num.length);
